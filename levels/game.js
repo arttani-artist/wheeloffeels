9b608274c396b81f811923fdf59c96d1287ce3f5
Arte <netra.sainath@gmail.com>
2026-03-12 02:42:09 -0500

    Update game.js

--- levels/game.js
+++ levels/game.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById("game")
 const ctx = canvas.getContext("2d")
-
+const playerSprite = new Image()
+playerSprite.src = "player.png"
 const tileSize = 64
 
 let player = {
